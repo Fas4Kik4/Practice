01ed05e9690e746e1e5c4c6c9be1e9743e5c99c5
FasKik <166402174+Fas4Kik4@users.noreply.github.com>
2025-02-12 12:53:21 +0200

    Add files via upload

--- js/app/pages/campaigns.js
+++ js/app/pages/campaigns.js
@@ -191,15 +191,15 @@
         <div id="spinner" v-if="loader"></div>
         <div class="wrapper">
             <div class="flex panel">
-                <div class="w202 al ptb20">
-                    <a class="btnS" href="#" @click.prevent="parent.formData={};$refs.new.active=1"><i class="fas fa-plus"></i> New</a>
+            <div class="w20 ptb30 tal">
+                    <h1>Campaigns</h1>
                 </div>
-                <div class="w60 ptb20 ac">
+                <div class="w600 ptb20 ac alc">
                     <input type="date" v-model="date" @change="get()" /> - 
                     <input type="date" v-model="date2" @change="get()" />
                 </div>
-                <div class="w202 ptb30 order2">
-                    <h1>Campaigns</h1>
+                <div class="w20 ptb20 alc2">
+                    <a class="btnS" href="#" @click.prevent="parent.formData={};$refs.new.active=1"><i class="fas fa-plus"></i> New</a>
                 </div>
             </div>
             
